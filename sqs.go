93c6820bd1b9c8fd8efb1c26860a71c034167e17
Matt <matt.ho@gmail.com>
2013-08-14 02:10:07 -0700

    - moved vars to const

--- sqs.go
+++ sqs.go
@@ -44,11 +44,14 @@ type SQSWriter struct {
 
 var (
 	// configure our sqs read settings
-	DEFAULT_TIMEOUT         time.Duration = 5 * time.Minute
-	RECV_ALL                              = []string{"All"}
-	RECV_MAX_MESSAGES                     = 10
-	RECV_VISIBILITY_TIMEOUT               = 900 // 15 minutes
-	DELETE_BATCH_SIZE                     = 10  // delete after theis many messages have been sent
+	DEFAULT_TIMEOUT time.Duration = 5 * time.Minute
+	RECV_ALL                      = []string{"All"}
+)
+
+const (
+	RECV_MAX_MESSAGES       = 10
+	RECV_VISIBILITY_TIMEOUT = 900 // 15 minutes
+	DELETE_BATCH_SIZE       = 10  // delete after theis many messages have been sent
 )
 
 func LookupQueue(queueName string) (*gosqs.Queue, error) {
