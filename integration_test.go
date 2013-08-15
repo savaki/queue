d7e6c0f914c28d5ee6e6e88339c306074bb9f70d
Matt <matt.ho@gmail.com>
2013-08-15 10:20:16 -0700

    - improved test

--- integration_test.go
+++ integration_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 var (
+	// a logger that can be used for testing
 	LOGGER = log.New(os.Stdout, "queue:", log.Ldate|log.Ltime)
 )
 
@@ -34,8 +35,17 @@ func TestIntegration(t *testing.T) {
 	select {
 	case message := <-input:
 		actual := make(map[string]string)
-		message.Unmarshal(actual)
+		message.Unmarshal(&actual)
 		message.OnComplete()
+
+		if len(actual) != len(expected) {
+			t.Fatalf("expected %+v; actual was %+v\n", expected, actual)
+		}
+		for key, value := range expected {
+			if actual[key] != value {
+				t.Fatalf("expected %+v; actual was %+v\n", value, actual[key])
+			}
+		}
 		<-time.After(timeout * 2)
 
 	case <-time.After(time.Second * 15):
